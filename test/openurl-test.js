d247e618b02b13cae7b701f0d23675cb3bf56a66
Senthil Maruthaiappan <senips@gmail.com>
2015-09-15 14:06:13 -0400

    fixed bugs

--- test/openurl-test.js
+++ test/openurl-test.js
@@ -4,14 +4,20 @@ var e2e = require('../lib/promiseaware');
 var wrapper = require('../lib/buildwrapper')
 
 
-e2e.describe('launch a browser and open some urls', function() {
+e2e.describe('launch a browser and open google', function() {
+
+    e2e.beforeEach(function() {
+        gBrowserClient.launch();
+    });
 
     e2e.it('open google url and read the title', function () {
 
       gBrowserClient.openUrl('http://google.com');
+      gBrowserClient.getTitle().then(function(title){
+              assert.equal(title, 'Google');
+      });
 
     });
-
 
 
 });
